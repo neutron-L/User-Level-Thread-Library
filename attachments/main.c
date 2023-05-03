99c6a73af81bddad6ea4448cd80751930d61d738
rda <2055017970@qq.com>
2023-05-03 22:55:30 +0800

    a test program

--- attachments/main.c
+++ attachments/main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include "my_pthread.h"
 
-void thread_run()
+int sum1, sum2;
+int arr[1000];
+int flag;
+
+void foo(void *)
 {
-  while (1)
-    printf("Other thread\n");
+    for (int i = 0; i < 500; ++i)
+    {
+        // printf("%d\n", my_pthread_self());
+        sum1 += arr[i];
+    }
+}
+
+
+void bar(void *)
+{
+    for (int i = 500; i < 1000; ++i)
+    {
+        // printf("%d\n", my_pthread_self());
+        sum2 += arr[i];
+    }
 }
 
 int main()
 {
-  struct itimerval new_value, old_value;
-  new_value.it_value.tv_sec = 0;
-  new_value.it_value.tv_usec = 500000;
-  new_value.it_interval.tv_sec = 0;
-  new_value.it_interval.tv_usec = 500000;
-  setitimer(ITIMER_REAL, &new_value, &old_value);
+    int x;
 
-  int x;
+    my_pthread_t tid1, tid2;
 
-  my_pthread_t thread;
+    for (int i = 0; i < 1000; ++i)
+        arr[i] = rand() % 10000;
+    my_pthread_create(&tid1, (void *)foo, (void *)NULL);
+    my_pthread_create(&tid2, (void *)bar, (void *)NULL);
+    
+    // my_pthread_join(tid1);
+    my_pthread_join(tid2);
 
-  my_pthread_create(&thread, (void *)thread_run, (void *)NULL);
+    printf("%d + %d = %d\n", sum1, sum2, sum1 + sum2);
 
-  while (1)
-    printf("Main thread\n");
-
-  return 0;
+    return 0;
 }
